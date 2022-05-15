617635f9e907ecef93ff465b3edec3d11b01f57f
beom-yun <scds0326@naver.com>
2022-05-15 22:49:49 +0900

    make User resolvers

--- server.js
+++ server.js
@@ -11,12 +11,25 @@ let tweets = [
   },
 ];
 
+let users = [
+  {
+    id: "1",
+    firstName: "SangBeom",
+    lastName: "Yun",
+  },
+  {
+    id: "2",
+    firstName: "Elon",
+    lastName: "Musk",
+  },
+];
+
 const typeDefs = gql`
   type User {
     id: ID!
-    username: String!
     firstName: String!
-    lastName: String
+    lastName: String!
+    fullName: String!
   }
   type Tweet {
     id: ID!
@@ -24,6 +37,7 @@ const typeDefs = gql`
     author: User
   }
   type Query {
+    allUsers: [User!]!
     allTweets: [Tweet!]!
     tweet(id: ID!): Tweet
     ping: String!
@@ -36,6 +50,9 @@ const typeDefs = gql`
 
 const resolvers = {
   Query: {
+    allUsers() {
+      return users;
+    },
     allTweets() {
       return tweets;
     },
@@ -57,6 +74,11 @@ const resolvers = {
       if (!tweet) return false;
       tweets = tweets.filter((tweet) => tweet.id !== id);
       return true;
+    },
+  },
+  User: {
+    fullName({ firstName, lastName }) {
+      return `${firstName} ${lastName}`;
     },
   },
 };
